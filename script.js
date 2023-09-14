12424dad3b608bd685a53d6728c213c9550ec134
Abhishek Roushan <abhishekashu23@gmail.com>
2023-09-14 20:48:01 +0530

    V1.1

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 const number = document.getElementById("number");
+const input = document.getElementById("input");
+const anim = document.querySelector(".anim");
+
+input.oninput = function () {
+  this.nextElementSibling.value = `${this.value}%`;
+};
+
+input.addEventListener("change", () => {
+  document.documentElement.style.setProperty(
+    "--keyWidth",
+    565 - (565 * input.value) / 100
+  );
+
+  anim.classList.remove("anim");
+
+  anim.offsetHeight;
+  anim.classList.add("anim");
+});
+
+// getComputedStyle(document.documentElement).getPropertyValue("--keyWidth");
 
 let counter = 0;
 
 setInterval(() => {
-  if (counter == 65) {
+  if (counter == input.value) {
     clearInterval();
   } else {
     counter++;
     number.innerHTML = `${counter}%`;
   }
-}, 30.76);
+}, 2000 / input.value);
